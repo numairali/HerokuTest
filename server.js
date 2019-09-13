8b3b82707f02dec2849494f7d5147a6c3fc1f87e
numairsvacademy <numair@sv.academy>
2019-09-13 12:31:03 -0600

    fix

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/HerokuTest'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/HerokuTest/index.html'));
 });
 console.log('console')
 // Start the app by listening on the default Heroku port
