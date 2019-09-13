74869f32972250a309b62131a1b1618cb4a06b46
numairsvacademy <numair@sv.academy>
2019-09-13 14:21:52 -0600

    fix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/HerokuTest'));
 
  
 app.listen(process.env.PORT || 8080);
 
-app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname , '/index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname , '/dist/HerokuTest/index.html'));
 });
 console.log('console')
