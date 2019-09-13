60daea520e6d256fa6c333876c92cb6648f0a012
numairsvacademy <numair@sv.academy>
2019-09-13 15:01:15 -0600

    fix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/bulky'));
+app.use(express.static('./dist/bulkyTest'));
 
  
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/bulky/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/bulkyTest/index.html'));
 });
 console.log('console')
