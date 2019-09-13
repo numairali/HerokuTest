0fe5087411a9eb7a304019e5e7f46e5e693a24ac
numairsvacademy <numair@sv.academy>
2019-09-13 12:58:50 -0600

    fix

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-//Install express server
+// Install express server
 const express = require('express');
-const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
+const path = require('path');
+
 app.use(express.static(__dirname + '/dist'));
+
  
-app.get('/*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.listen(process.env.PORT || 8080);
+
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
 });
 console.log('console')
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
